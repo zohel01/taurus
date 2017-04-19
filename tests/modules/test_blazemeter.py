d3b6226543ecba4d35d29f9760527f9b8ab15396
greyfenrir <grey.fenrir@gmail.com>
2017-04-19 15:54:29 +0300

    small......

--- tests/modules/test_blazemeter.py
+++ tests/modules/test_blazemeter.py
@@ -13,41 +13,15 @@ class TestBZAObject(BZTestCase):
         obj = User()
         obj.ping()
 
-    def test_request(self):
-        obj = BZAObject()
-        try:
-            obj._request('https://a.blazemeter.com/api/v4/web/version', data={"test": 1})
-            self.fail()
-        except TaurusNetworkError:
-            pass
-
-
 class TestCloudProvisioningOld(BZTestCase):
     def test_case1(self):
         mock = BZMock()
 
-        mock.mock_get.update({
-            'https://a.blazemeter.com/api/v4/masters/1/sessions': {"result": {"sessions": []}},
-            'https://a.blazemeter.com/api/v4/masters/1/full': {"result": {"sessions": []}},
-            'https://a.blazemeter.com/api/v4/masters/1': {"result": {"note": "message"}},
-            'https://a.blazemeter.com/api/v4/masters/1/status': [
-                {"result": {"id": 1, "status": "CREATE"}},
-                {"result": {"id": 1, "status": "ENDED", "progress": 101}}
-            ],
-        })
+        mock.mock_get.update({})
 
-        mock.mock_post = {
-            'https://a.blazemeter.com/api/v4/projects': {"result": {"id": 1}},
-            'https://a.blazemeter.com/api/v4/tests': {"result": {"id": 1}},
-            'https://a.blazemeter.com/api/v4/tests/1/files': {"result": None},
-            'https://a.blazemeter.com/api/v4/tests/1/start': {"result": {"id": 1}},
-            'https://a.blazemeter.com/api/v4/masters/1/stop': {"result": None},
-            'https://a.blazemeter.com/api/v4/masters/1/public-token': {"result": {"publicToken": "token"}},
-        }
+        mock.mock_post = {}
 
-        mock.mock_patch = {
-            'https://a.blazemeter.com/api/v4/tests/1': {"result": {}}
-        }
+        mock.mock_patch = {}
 
         prov = CloudProvisioning()
         prov.browser_open = None
