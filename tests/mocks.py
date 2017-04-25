6e7fdea7fff189329a0c4a32db6f9472b910bc58
greyfenrir <grey.fenrir@gmail.com>
2017-04-25 13:33:05 +0300

    smaller.

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,18 +1,13 @@
 """ test """
 import logging
-import os
-import random
 import sys
-import tempfile
 from logging import Handler
 
 import requests
 
-from bzt.engine import Engine, Configuration, FileLister, HavingInstallableTools
+from bzt.engine import Engine, FileLister, HavingInstallableTools
 from bzt.engine import Provisioning, ScenarioExecutor, Reporter
-from bzt.modules.aggregator import ResultsReader
 from bzt.utils import load_class, to_json
-from tests import random_sample
 
 try:
     from exceptions import KeyboardInterrupt
@@ -24,69 +19,14 @@ except ImportError:
 class EngineEmul(Engine):
     def __init__(self):
         super(EngineEmul, self).__init__(logging.getLogger(''))
-        self.config.get('settings')['artifacts-dir'] = os.path.dirname(__file__) + "/../build/test/%Y-%m-%d_%H-%M-%S.%f"
-        self.config.get('settings')['check-updates'] = False
-        self.create_artifacts_dir()
-        self.config.merge({"provisioning": "local"})
-        self.config.merge({"modules": {"mock": ModuleMock.__module__ + "." + ModuleMock.__name__}})
-        self.finalize_exc = None
-        self.was_finalize = False
-
-    def dump_config(self):
-        """ test """
-        fname = tempfile.mkstemp()[1]
-        self.config.dump(fname, Configuration.JSON)
-        with open(fname) as fh:
-            logging.debug("JSON:\n%s", fh.read())
-
+        pass
 
 class ModuleMock(ScenarioExecutor, Provisioning, Reporter, FileLister, HavingInstallableTools):
     """ mock """
 
     def __init__(self):
         super(ModuleMock, self).__init__()
-        self.postproc_exc = None
-        self.check_exc = None
-        self.prepare_exc = None
-        self.startup_exc = None
-        self.shutdown_exc = None
-
-        self.check_iterations = sys.maxsize
-
-        self.was_shutdown = False
-        self.was_startup = False
-        self.was_prepare = False
-        self.was_check = False
-        self.was_postproc = False
-
-        self.is_has_results = False
-
-
-    def get_exc(self, param):
-        """
-        :type param: str
-        :return:
-        """
-        name = self.settings.get(param, "")
-        if name:
-            cls = load_class(name)
-            return cls()
-        return None
-
-    def resource_files(self):
-        """
-
-
-        :return:
-        """
-        self.execution.get('files', []).append(__file__)
-        return [__file__]
-
-    def has_results(self):
-        return self.is_has_results
-
-    def install_required_tools(self):
-        self.log.debug("All is good")
+        pass
 
 
 class RecordingHandler(Handler):
